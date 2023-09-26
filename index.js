e5f86ad3efcfd901643bcb8ecbc731034dd6a020
Denis McMahon <denismcmahon@gmail.com>
2023-09-26 16:13:28 +0100

    Setting up port variable to work on Heroku

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ app.post('/calculate', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+    console.log(`Server running on port: ${port}`);
 });
